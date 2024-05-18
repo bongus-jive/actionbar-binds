42fccfaf51d32b452908b5610955b5af47bb5e94
patman <27874300+patmanf@users.noreply.github.com>
2024-05-18 13:53:33 -0500

    fixed prev. group bind always scrolling back 2 groups

--- pat_actionbarbinds.lua
+++ pat_actionbarbinds.lua
@@ -1,14 +1,20 @@
+local lastGroup
+
 function init()
   if not starExtensions and script.updateDt() ~= 0 then
     script.setUpdateDelta(0)
     sb.logWarn("'Action Bar Binds' requires StarExtensions - https://github.com/StarExtensions/StarExtensions/releases")
   end
+
+  lastGroup = player.actionBarGroup()
 end
 
 function update()
   if input.bindDown("pat_actionbarbinds", "prev") then
-    player.setActionBarGroup(player.actionBarGroup() - 2)
+    player.setActionBarGroup(lastGroup - 1)
   end
+
+  lastGroup = player.actionBarGroup()
   
   for i = 7, 12 do
     if input.bindDown("pat_actionbarbinds", tostring(i)) then
